3412fcda0c66de3f8f80a052ea27b7247e18e65f
Seonhyeong Kim <tjsgud2564@naver.com>
2022-12-07 10:23:03 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -6,25 +6,25 @@ function solution(keyinput, board) {
   keyinput.forEach((item) => {
     if (item === "left") {
       if (x === -xLimit) {
-        x = x;
+    
       } else {
         x -= 1;
       }
     } else if (item === "right") {
       if (x === xLimit) {
-        x = x;
+ 
       } else {
         x += 1;
       }
     } else if (item === "up") {
       if (y === yLimit) {
-        y = y;
+    
       } else {
         y += 1;
       }
     } else if (item === "down") {
       if (y === -yLimit) {
-        y = y;
+    
       } else {
         y -= 1;
       }
